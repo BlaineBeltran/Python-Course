635f381bceeebf450cad27d1bfec428ac8e29f3c
BlaineBeltran <55524257+ComputerEng21@users.noreply.github.com>
2020-06-29 19:24:34 -0500

    Added program to calculate Payments

---
+++ Programming for Everybody Files/payRate.py
@@ -0,0 +1,6 @@
+# This program will calculate your Payment amount
+
+hrs = input("How many hours do you have? ")
+rate = input("What is your hourly rate? ")
+pay = float(hrs) * float(rate)
+print(pay)
